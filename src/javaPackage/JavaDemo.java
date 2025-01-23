7f404180f502781231e741e952d298355512f000
chakredhanraj123 <chakredhanraj123@gmail.com>
2025-01-23 13:06:41 +0530

    As Lead Sharing Project

---
+++ src/javaPackage/JavaDemo.java
@@ -0,0 +1,10 @@
+package javaPackage;
+
+public class JavaDemo {
+
+	public static void main(String[] args) {
+
+		System.out.println("As a Lead I have created one project  .Its name is:  JavaProject ");
+	}
+
+}
