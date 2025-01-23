2508f834ba108fcd9ccad488127e72c439811080
sampatpatil012 <sampatpatil012@gmail.com>
2025-01-23 13:32:14 +0530

    I have created Demo PAckage

--- src/demoPackage/Demo.java
+++ src/demoPackage/Demo.java
@@ -3,6 +3,7 @@ package demoPackage;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println(" =================================  ");
+		System.out.println(" =================================  ");
 	}
 
 }
